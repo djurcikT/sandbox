62cfacf977649808ca5883d842e2b90b388bd25a
Tamara <tamaradjurcik@gmail.com>
2024-05-17 14:44:38 +0200

    Update Header.js
    TabMenu change

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,34 @@ import "primeicons/primeicons.css";
 import React, { useState } from "react";
 import "primeflex/primeflex.css";
 import "../src/Main.css";
+import { TabMenu } from "primereact/tabmenu";
 
 export function Header(props) {
+  const menuItems = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+    },
+    {
+      label: "User Management",
+      icon: "pi pi-user",
+    },
+    {
+      label: "Product Management",
+      icon: "pi pi-question",
+    },
+  ];
+
   return (
-    <div className="Title text-center">
-      <h1>{props.headerLabel}</h1>
+    <div className="HeaderContainer">
+      <div className="Title text-center">
+        <h1>{props.headerLabel}</h1>
+      </div>
+      <div className="Meni">
+        <div className="card">
+          <TabMenu model={menuItems} />
+        </div>
+      </div>
     </div>
   );
 }
