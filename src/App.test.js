bf36fad5a39770b666f533af92962cc1ddad47d5
Tamara <tamaradjurcik@gmail.com>
2024-05-23 10:04:56 +0200

    Running tests
    Side quest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Numbers and strings arithmetics test", () => {
+  const br1 = 10;
+  const br2 = 20;
+  const br3 = "10";
+  const br4 = "20";
+
+  const zbir1 = br1 + br2;
+  const zbir2 = br3 + br4;
+  const zbir3 = br1 + br3;
+  const zbir4 = br4 + br2;
+
+  console.log("Zbir 1 = ", zbir1);
+  console.log("Zbir 2 = ", zbir2);
+  console.log("Zbir 3 = ", zbir3);
+  console.log("Zbir 4 = ", zbir4);
+
+  expect(zbir4).toBe(2020);
 });
